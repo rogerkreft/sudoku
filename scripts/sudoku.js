679f4f0a97f294acf976f16830d1278511de650c
rogerkreft <roger.kreft@web.de>
2020-04-17 20:54:36 +0200

    :construction: sudoku data structures added (WIP)

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,18 +1,48 @@
 
+var squares = new Map()
+var rows = new Map()
+var columns = new Map()
+
 function initSudoku() {
-    var elements = document.querySelectorAll('.sudoku input')
-    for (i = 0; i < elements.length; i++) {
-        var element = elements[i]
-        element.addEventListener('keypress', keyPressed)
+    var divs = document.querySelectorAll('.sudoku>*')
+    for (i = 0; i < divs.length; i++) {
+        var div = divs[i]
+        var inputs = div.getElementsByTagName('input')
+        for (j = 0; j < inputs.length; j++) {
+            var input = inputs[j]
+            input.addEventListener('keypress', e => validateInput(e, keyPressed))
+            input.addEventListener('keyup', e => validateInput(e, keyUp))
+            insert(i, j, input)
+        }
+    }
+}
+
+function validateInput(e, f) {
+    var valid = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    if (valid.includes(e.key)) {
+        f(e)
+    } else {
+        e.preventDefault()
     }
 }
 
 function keyPressed(e) {
-    var keyCode = e.keyCode
-    if (keyCode < 49 || keyCode > 57) {
-        e.preventDefault()
-    } else {
-        var target = e.target
-        target.value = ''
+    var target = e.target
+    target.value = ''
+}
+
+function keyUp(e) {
+    console.log(squares)
+}
+
+function insert(i, j, input) {
+    insertSquare(i, input)
+}
+
+function insertSquare(i, input) {
+    if (!squares.has(i)) {
+        squares.set(i, new Array())
     }
+    var square = squares.get(i)
+    square.push(input)
 }
